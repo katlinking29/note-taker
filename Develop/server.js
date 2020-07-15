f738b64b015cf8943b0743f77e0110106e2f7eb1
Katlin King <katlinking29@gmail.com>
2020-07-14 21:04:14 -0400

    updated / instead of *

--- Develop/server.js
+++ Develop/server.js
@@ -73,7 +73,7 @@ app.delete("/api/notes/:id", function(req, res){
 
 
 // return the index.html file
-app.get("*", function(req, res) {
+app.get("/", function(req, res) {
   res.sendFile(path.join(__dirname, "/public/index.html"));
 });
 
